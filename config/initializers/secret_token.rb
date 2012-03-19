723b009547aeb89ebb7521e5b95fd4589a32801b
Farai <farai@webshopapps.com>
2012-03-19 21:10:18 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bugtracker::Application.config.secret_token = 'd5fb4b8d58d80a77e959a75eb4392187e1a5eb79fbd88907fa9ba579ed96b972e4df816cfaaf14921f4576150979d5823d6dd2d853a10183a8ac1e0ecceff6e5'
