9ee1059b040bc3ddbe87f67df9abd34072c03536
Farai <farai@webshopapps.com>
2012-04-17 08:23:15 +0100

    added status icons for open and closed bug report cases, added select field to set bug status, added link to homepage

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -1,2 +1,10 @@
 module BugsHelper
+  def status_icon(type)
+    case type
+       when 'Open' 
+         then "label label-important"   
+       when 'Resolved'
+         then "label label-success"
+    end
+  end
 end
