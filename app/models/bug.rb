475f7127bd51cf532fd5f049bb2f5396a2b66d7e
Farai <farai@webshopapps.com>
2012-06-14 09:58:35 +0100

    changed bug form to use formtastic, added additional model validation, changed main menu link to point to project list not bugs

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,5 +2,7 @@ class Bug < ActiveRecord::Base
   belongs_to :project
   has_many :comments, :dependent => :destroy
   
+  validates_presence_of :author ,:email , :description, :status, :priority, :project_id
+  
 
 end
